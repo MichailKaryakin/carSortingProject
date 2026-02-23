8697b08ea7acef7000ca937846411d3c40f5b01e
Kirill <kirill532692@mail.ru>
2026-02-23 18:24:39 +0300

    Для пакета model в класс Car добавлены: приватный конструктор, три компаратора, вложенный класс билдер и переопределения методов equals, hashCode и toString

--- src/main/java/org/example/model/Car.java
+++ src/main/java/org/example/model/Car.java
@@ -3,6 +3,9 @@ package org.example.model;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.util.Comparator;
+import java.util.Objects;
+
 @SuppressWarnings("ClassCanBeRecord")
 public class Car {
     private final int power;
@@ -18,7 +21,12 @@ public class Car {
         this.year = year;
     }
 
-    // здесь приватный конструктор, принимающий билдер
+    // приватный конструктор, принимающий билдер
+    private Car(Builder builder) {
+        this.power = builder.power;
+        this.model = builder.model;
+        this.year = builder.year;
+    }
 
     public int getPower() {
         return power;
@@ -32,9 +40,68 @@ public class Car {
         return year;
     }
 
-    // три компаратора для сортировки по трём разным полям compareByPower(), compareByModel(), compareByYear()
+    // три компаратора для сортировки по трём разным полям compareByPower(),
+    // compareByModel(), compareByYear()
+    public static Comparator<Car> compareByPower() {
+        return Comparator.comparingInt(Car::getPower);
+    }
+
+    public static Comparator<Car> compareByModel() {
+        return Comparator.comparing(Car::getModel);
+    }
+
+    public static Comparator<Car> compareByYear() {
+        return Comparator.comparingInt(Car::getYear);
+    }
 
     // вложенный класс билдер
+    public static class Builder {
+
+        private int power;
+        private String model;
+        private int year;
+
+        public Builder power(int power) {
+            this.power = power;
+            return this;
+        }
+
+        public Builder model(String model) {
+            this.model = model;
+            return this;
+        }
+
+        public Builder year(int year) {
+            this.year = year;
+            return this;
+        }
+
+        public Car build() {
+            return new Car(this);
+        }
+    }
 
     // equals, hashCode, toString
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Car car)) return false;
+        return power == car.power &&
+                year == car.year &&
+                Objects.equals(model, car.model);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(power, model, year);
+    }
+
+    @Override
+    public String toString() {
+        return "Car: " +
+                "power = " + power +
+                ", model = '" + model +
+                ", year = " + year +
+                '}';
+    }
 }
