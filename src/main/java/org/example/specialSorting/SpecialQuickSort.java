71d319c530e09b1718414228ddc74f1a54d1a57f
Yakov Kuklin <grieen.94@mail.ru>
2026-03-10 20:18:17 +0300

    добавил SpecialQuickSort

---
+++ src/main/java/org/example/specialSorting/SpecialQuickSort.java
@@ -0,0 +1,19 @@
+package org.example.specialSorting;
+
+import org.example.model.Car;
+import org.example.sorting.QuickSort;
+
+import java.util.Comparator;
+import java.util.List;
+
+import static org.example.specialSorting.SortUtils.createEvenList;
+import static org.example.specialSorting.SortUtils.insertSortedList;
+
+public class SpecialQuickSort extends QuickSort {
+    @Override
+    public void sort(List<Car> cars, Comparator<Car> comparator) {
+        List<Car> evenList = createEvenList(cars, comparator);
+        super.sort(evenList, comparator);
+        insertSortedList(cars, evenList, comparator);
+    }
+}
